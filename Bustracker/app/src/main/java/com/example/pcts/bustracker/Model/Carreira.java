adb85f31bf6fadd695bdab21c210cc364ca0f2c8
Ahlid <tiagoasmont@gmail.com>
2016-11-24 21:42:33 +0000

    Modelo de dados da informação (tested)

--- Bustracker/app/src/main/java/com/example/pcts/bustracker/Model/Carreira.java
+++ Bustracker/app/src/main/java/com/example/pcts/bustracker/Model/Carreira.java
@@ -13,7 +13,88 @@ import java.util.List;
 public class Carreira {
     private int numero;
     private List<Paragem> paragens;
-    private Date partida;
-    private String detalhe;
+    private List<Autocarro> partidas;
 
+
+    public Carreira(int numero) {
+
+        this.numero = numero;
+        this.paragens = new ArrayList<Paragem>();
+        this.partidas = new ArrayList<Autocarro>();
+
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public List<Paragem> getParagens() {
+        return paragens;
+    }
+
+    public void setParagens(List<Paragem> paragens) {
+        this.paragens = paragens;
+    }
+
+    public List getPartida() {
+        return partidas;
+    }
+
+    public void addPartida(Autocarro partida) {
+        this.partidas.add(partida);
+    }
+
+    public boolean addParagem(Paragem e){
+        return this.paragens.add(e);
+    }
+    public Paragem removeParagem(int i){
+        return this.paragens.remove(i);
+    }
+    public boolean removeParagem(Paragem p){
+        return this.paragens.remove(p);
+    }
+
+    public Date getDataProximaCarreira(){
+
+        Date proxima = null;
+        Date actual = new Date();
+        for (Autocarro a : this.partidas){
+            if(a.getPartida().after(actual)){
+                proxima = a.getPartida() ;
+                break;
+            }
+        }
+
+        return proxima;
+    }
+
+    public List<Autocarro> getProximasPartidas(){
+
+        List<Autocarro> proximas = new ArrayList<>();
+        Date actual = new Date();
+
+        for (Autocarro a : this.partidas){
+            if(a.getPartida().after(actual)){
+                proximas.add(a);
+
+                if(proximas.size()> 4)
+                break;
+            }
+        }
+
+        return proximas;
+    }
+
+    @Override
+    public String toString() {
+        return "Carreira{" +
+                "numero=" + numero +
+                ", paragens=" + paragens +
+                ", partidas=" + partidas +
+                '}';
+    }
 }
