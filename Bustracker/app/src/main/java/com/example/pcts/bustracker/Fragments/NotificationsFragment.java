a446ea33850fed6438a612d6af69e198b5d97337
Ahlid <tiagoasmont@gmail.com>
2016-12-07 16:08:00 +0000

    Db notificações e gestor notificações

--- Bustracker/app/src/main/java/com/example/pcts/bustracker/Fragments/NotificationsFragment.java
+++ Bustracker/app/src/main/java/com/example/pcts/bustracker/Fragments/NotificationsFragment.java
@@ -6,7 +6,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.pcts.bustracker.Managers.GestorInformacao;
+import com.example.pcts.bustracker.Managers.GestorNotificacao;
+import com.example.pcts.bustracker.Model.Notificacao;
 import com.example.pcts.bustracker.R;
 
 
@@ -24,6 +29,23 @@ public class NotificationsFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_notifications, container, false);
         ((Toolbar) getActivity().findViewById(R.id.toolbar)).setTitle("Notificações");
+
+
+
+        Button b = (Button) rootView.findViewById(R.id.button4);
+
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Notificacao n = new Notificacao(GestorInformacao.getInstance().findParagemById(1),GestorInformacao.getInstance().findCarreiraById(1),5);
+                GestorNotificacao.getInstance().addNotificacao(n);
+
+               Toast.makeText(getContext(),n.toString(),Toast.LENGTH_LONG).show();
+
+            }
+        });
+
 
         // Inflate the layout for this fragment
         return rootView;
--- Bustracker/app/src/main/java/com/example/pcts/bustracker/Managers/GestorNotificacao.java
+++ Bustracker/app/src/main/java/com/example/pcts/bustracker/Managers/GestorNotificacao.java
@@ -1,5 +1,10 @@
 package com.example.pcts.bustracker.Managers;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.widget.Toast;
+
+import com.example.pcts.bustracker.Database.DatabaseNotificacoes;
 import com.example.pcts.bustracker.Model.Carreira;
 import com.example.pcts.bustracker.Model.Notificacao;
 import com.example.pcts.bustracker.Model.Paragem;
@@ -14,23 +19,50 @@ import java.util.List;
 public class GestorNotificacao {
 
     private static GestorNotificacao instance;
+    private DatabaseNotificacoes db;
 
     private List<Notificacao> notificacoes;
 
-    private GestorNotificacao(){
+    private GestorNotificacao(Context context){
         this.notificacoes = new ArrayList<>();
+        this.db = new DatabaseNotificacoes(context);
+
+        Cursor c = db.getAllData();
+
+        while (c.moveToNext()){
+
+            int id = c.getInt(c.getColumnIndex(DatabaseNotificacoes.COL_1));
+            Carreira carreira = GestorInformacao.getInstance().findCarreiraById(c.getInt(c.getColumnIndex(DatabaseNotificacoes.COL_2)));
+            Paragem paragem = GestorInformacao.getInstance().findParagemById(c.getInt(c.getColumnIndex(DatabaseNotificacoes.COL_3)));
+            int minutos = c.getInt(c.getColumnIndex(DatabaseNotificacoes.COL_4));
+
+            this.notificacoes.add(new Notificacao(id,paragem, carreira,minutos ));
+        }
+
+
     }
 
-    public static GestorNotificacao  getInstance(){
+    public static GestorNotificacao  getInstance(Context context){
         if(instance == null){
-            instance = new GestorNotificacao();
+            instance = new GestorNotificacao(context);
         }
 
         return instance;
     }
 
+    public static GestorNotificacao getInstance(){
+        return instance;
+    }
+
     public boolean addNotificacao(Notificacao n){
         boolean res = (this.notificacoes.contains(n)) ? false : this.notificacoes.add(n);
+
+        if(res){
+
+
+           Long r= db.insertData(n.getCarreira().getId(), n.getParagem().getId(), n.getMinutos());
+            n.setId(r);
+        }
 
         return res;
     }
