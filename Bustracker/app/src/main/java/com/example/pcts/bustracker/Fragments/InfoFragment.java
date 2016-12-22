adfaf9b447ac450bbcb3d0e5eb0674db2d262868
Ricardo Morais <moraispgsi@gmail.com>
2016-12-22 21:36:11 +0000

    Limpeza do layout do fragmento de informações

--- Bustracker/app/src/main/java/com/example/pcts/bustracker/Fragments/InfoFragment.java
+++ Bustracker/app/src/main/java/com/example/pcts/bustracker/Fragments/InfoFragment.java
@@ -26,149 +26,36 @@ import java.util.Arrays;
 
 public class InfoFragment extends Fragment {
 
-    private final String PARAGENS;
-    private final String CARREIRAS;
-    private final String CARREIRAS_PARAGEM;
-    private String selected;
+    private final String PARAGENS = "Paragens";
+    private final String CARREIRAS = "Carreiras";
 
-
-    String[] items;
-
-    ArrayList<String> listItems;
-
-    ArrayAdapter<String> adapter;
-
-    ListView listView;
-
-    EditText editText;
-    View rootView;
-
-    public InfoFragment(){
-
-        PARAGENS = "Paragens";
-        CARREIRAS = "CarreiraActivity";
-        CARREIRAS_PARAGEM = "CarreiraActivity & ParagemActivity";
-        selected =CARREIRAS;
-    }
+    private View rootView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-         rootView = inflater.inflate(R.layout.fragment_information, container, false);
+        rootView = inflater.inflate(R.layout.fragment_information, container, false);
 
+        Toolbar toolbar =  (Toolbar) getActivity().findViewById(R.id.toolbar);
+        toolbar.setTitle("Infomações");
 
-        ((Toolbar) getActivity().findViewById(R.id.toolbar)).setTitle("Infomações");
-
-   
-
-        TabHost host = (TabHost) rootView.findViewById( R.id.tabInfo);
+        TabHost host = (TabHost) rootView.findViewById(R.id.tabInfo);
         host.setup();
 
-     //TODO: resolver problema das tabs em tablets
-
-        //Tab 1
+        //Tab Carreiras
         TabHost.TabSpec spec = host.newTabSpec(CARREIRAS);
         spec.setContent(R.id.carreiras);
         spec.setIndicator(CARREIRAS);
         host.addTab(spec);
 
-        //Tab 2
+        //Tab Paragens
         spec = host.newTabSpec(PARAGENS);
         spec.setContent(R.id.paragens);
         spec.setIndicator(PARAGENS);
         host.addTab(spec);
 
-        spec = host.newTabSpec(CARREIRAS_PARAGEM);
-        spec.setContent(R.id.carreirasParagens);
-        spec.setIndicator(CARREIRAS_PARAGEM);
-        host.addTab(spec);
-        init(selected);
-
-
-        editText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                ((TextView) rootView.findViewById(R.id.textView4)).setText(charSequence);
-                if (!charSequence.toString().equals("")) {
-                    listItems.clear();
-                    for (String item : items) {
-
-                        if (item.contains(charSequence.toString())) {
-
-                            listItems.add(item);
-
-                        }
-                    }
-
-                } else {
-                    init(selected);
-                }
-            }
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        });
-        // Inflate the layout for this fragment
-
-        host.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
-            @Override
-            public void onTabChanged(String s) {
-                ((TextView) rootView.findViewById(R.id.textView4)).setText(s);
-
-               if(s.equals(PARAGENS)){
-                   selected = PARAGENS;
-               }else if(s.equals(CARREIRAS)){
-                   selected = CARREIRAS;
-               }else {
-                   selected = CARREIRAS_PARAGEM;
-               }
-
-               init(selected);
-            }
-        });
-
         return rootView;
-    }
-
-    private void init(String tab){
-
-        initHistory(tab);
-        initSearch(tab);
 
     }
 
-    private void initSearch(String tab){
-        listView= (ListView) rootView.findViewById(R.id.searchCarreirasResult);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                ((TextView) rootView.findViewById(R.id.textView4)).setText(i+"");
-
-            }
-        });
-
-        editText= (EditText) rootView.findViewById(R.id.searchCarreiras);
-
-        if(tab.equals(CARREIRAS)) {
-            items = new String[]{"TST 701", "TST 654", "TST 780", "TST 781"};
-        }else{
-            items = new String[]{"Loja Cidadão", "Mercado", "IPS", "Pontes"};
-        }
-        listItems=new ArrayList<>(Arrays.asList(items));
-
-        adapter=new ArrayAdapter<String>(getActivity().getApplicationContext(),
-                R.layout.list_item, R.id.txt, listItems);
-
-        listView.setAdapter(adapter);
-    }
-
-    private void initHistory(String tab){
-        //TODO:fazer o metodo
-    }
 }
